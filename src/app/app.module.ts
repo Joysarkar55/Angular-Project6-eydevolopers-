2b23b0cd2d04ad6bb5c4f3f391e9565d04f72ee0
JS952001 <sarkarpritam871@gmail.com>
2023-04-05 13:03:06 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{ ModuleModule } from './module/module.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UniversityModule } from './university/university.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModuleModule,
+    UniversityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/university/university.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentComponent } from './student/student.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    StudentComponent
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path:'student', component: StudentComponent }
+    ])
+  ]
+})
+export class UniversityModule { }
